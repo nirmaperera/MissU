f98a90307bef8b5defd11cd128f5bfdb394354ec
jc <rogerfrontend@gmail.com>
2019-01-19 11:35:51 -0500

    Setup pg addons

--- Server/models/db.js
+++ Server/models/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize("postgres://postgres:admin123@localhost/postgres");
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:admin123@localhost/postgres");
 
 
 module.exports = db
