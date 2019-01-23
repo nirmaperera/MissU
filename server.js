4678b5dd63c7124d59fdb267b963ed8328e0c133
jc <rogerfrontend@gmail.com>
2019-01-22 21:51:13 -0500

    Setup for heroku

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const path = require("path")
-const bodyParse = require("body-parser")
-const { MissingPeople } = require("./models")
-const { User } = require("./models")
+const path = require("path");
+const bodyParse = require("body-parser");
+const { MissingPeople } = require("./Server/models");
+const { User } = require("./Server/models");
 const port = process.env.PORT || 3000;
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const keys = require('./config/keys');
-require('./services/passport');
+const keys = require('./Server/config/keys');
+require('./Server/services/passport');
 const app = express();
 app.use(
   cookieSession({
@@ -21,12 +21,12 @@ app.use(passport.session());
 // below two lines can be refactor to be third line
 // const authRoutes = require('./routes/authRoutes');
 // authRoutes(app);
-require('./routes/authRoutes')(app);
+require('./Server/routes/authRoutes')(app);
 
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded());
 
-app.use("/api", require("./api"));
+app.use("/api", require("./Server/api"));
 // {force: true} will drop the table if it already exists
 MissingPeople.sync().then(() => console.log("missing people table"));
 User.sync().then(() => console.log("User table"));
@@ -39,6 +39,12 @@ User.sync().then(() => console.log("User table"));
 //   })
 //   // Express will serve up the index.html file if ..
 // }
+if(process.env.NODE_ENV === 'production'){
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
