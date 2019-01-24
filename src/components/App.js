ace8d3479ee82dfc2b973f1872694b1fda5d9f9a
jc <rogerfrontend@gmail.com>
2019-01-24 10:54:39 -0500

    case not working

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import {BrowserRouter,browserHistory, Route, Switch} from "react-router-dom";
 import Landing from "./Landing";
-import About from "./About";
+
 import View from "./View";
 import MissingForm from "./FormConnect";
 import MissingCards from "./CardsConnect";
 import Login from "./Login";
-import Signup from "./Signup";
+
 import Case from "./Case"
 import Errors from "./Errors";
 import Header from "./Header";
@@ -34,7 +34,6 @@ class App extends Component {
                 <Route path ="/missingForm" component={MissingForm}/>
                 <Route path = "/case/:case_id" component={Case}/>
                 <Route path="/Login" component={Login} />
-                <Route path="/Signup" component={Signup} />
                 <Route path="/case" component={Case}/>
 
               </div>
