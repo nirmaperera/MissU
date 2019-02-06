9d111e5e0ec0a0ee27abeb5d82e5c5c77487dcce
Nirma Perera <nirmaperera23@gmail.com>
2019-02-06 00:00:43 -0500

    fixed styles on missingCards.js

--- src/components/MissingForm.js
+++ src/components/MissingForm.js
@@ -22,7 +22,7 @@ class MissingForm extends Component{
             lastname: "",
             computedmissingmaxage: "",
             dateoflastcontact: "",
-
+            showResults:true,
             namus2number: ""
 
         };
@@ -61,109 +61,54 @@ class MissingForm extends Component{
 
     handleSearch(e){
         e.preventDefault();
-
+        this.setState({ showResults: false});
         this.props.searchThunk(this.state)
-        console.log(this.state.gender)
+        console.log(this.state)
     }
 
 
     render(){
         return(
-<<<<<<< HEAD
-            <div>
-          <form>
-            <div>
-               <div className="form-title">
-                  <h1> Search for a Missing Person</h1>
-                </div>
-                <div className="form-Missing">
-                <div>
-                    <label htmlFor="firstName" >First Name:</label>
-                    <input type="text" placeholder="Corceil" onChange={this.handleChangeFName.bind(this)}/>
-=======
-            <div className ="row">
-          <form  className = "col s12">
-            <div className = "row">
-                <div className="input-field col s6">
+             <div>
+                 <form>
+                     <div>
+                         <div className="form-title">
+                            <h1> Search for a Missing Person</h1>
+                            <hr className="missing"/>
+                         </div>
+                         <div className="form-Missing">
+                                <div>
+                                    <label htmlFor="firstName" >First Name:</label>
+                                    <input type="text" placeholder="Corceil" onChange={this.handleChangeFName.bind(this)}/>
+                                </div>
+                                <div>
+                                    <label htmlFor="lastname"  >Last Name:</label>
+                                    <input type="text" placeholder="Howard" onChange={this.handleChangeLName.bind(this)}/>
+                                </div>
+                    
+                                <div>
+                                    <label htmlFor="statedisplaynameoflastcontact" >State Display Name of Last Contact: </label>
+                                    <input type="text" placeholder="MI" onChange={this.handleChangeState.bind(this)}/>
+                               </div>
+                                <div>
+                                    <label htmlFor="gender" >Gender: </label>
+                                    <input type="text" placeholder="Male" onChange={this.handleChangeGender.bind(this)}/>
+                                </div>
+                                <div>
+                                    <button type="button" className="button-missing" onClick={this.handleSearch.bind(this)}>Search</button>
+                                    
+                                </div>
+                             </div>
 
-                    <input id="first_name" type="text" className="validate" onChange={this.handleChangeFName.bind(this)}/>
-                    <label htmlFor="first_name">First Name:</label>
-
->>>>>>> 5958bcdb9920a6fe947c174cc80d279dcee223cc
-                </div>
-                <div className="input-field col s6">
-                    <input id="middle_name" type="text" className="validate" onChange={this.handleChangeMName.bind(this)}/>
-                    <label htmlFor="middle_name" >Middle Name:</label>
-                </div>
-                <div className="input-field col s6">
-                    <input id="last_name" type="text" className="validate" onChange={this.handleChangeLName.bind(this)}/>
-                    <label htmlFor="last_name"  >Last Name:</label>
-
-                </div>
-                <div className="input-field col s6">
-                    <input id="age" type="text" className="validate"  onChange={this.handleChangeAge.bind(this)}/>
-                    <label htmlFor="age" >Age:</label>
-
-                </div>
-                <div className="input-field col s6">
-                    <input id="dateoflastcontact" type="text" className="validate" onChange={this.handleChangeDate.bind(this)}/>
-                    <label htmlFor="dateoflastcontact" >Date of Last Contact:</label>
-
-                </div>
-
-                <div className="input-field col s6">
-                    <label htmlFor="cityoflastcontact" >City of Last Contact:</label>
-                    <input type="text"  required onChange={this.handleChangeCity.bind(this)}/>
-                </div>
-
-                <div className="input-field col s6">
-                    <label htmlFor="countydisplaynameoflastcontact" >County Display Name of Last Contact:</label>
-                    <input type="text"  onChange={this.handleChangeCounty.bind(this)}/>
-                </div>
-
-                <div className="input-field col s6">
-                    <label htmlFor="raceethnicity">Race/Ethnicity: </label>
-<<<<<<< HEAD
-                    <input type="text" placeholder="Black / African American" onChange={this.handleChangeRace.bind(this)}/>
-=======
-                    <input type="text"  onChange={this.handleChangeRace.bind(this)}/>
->>>>>>> 5958bcdb9920a6fe947c174cc80d279dcee223cc
-                </div>
-
-                <div className="input-field col s6">
-                    <label htmlFor="statedisplaynameoflastcontact" >State Display Name of Last Contact: </label>
-                    <input type="text"  onChange={this.handleChangeState.bind(this)}/>
-                </div>
+                         </div>
+                     </form>
 
 
-<<<<<<< HEAD
-                <button type="button" className="button-missing" onClick={this.handleSearch.bind(this)}>Search</button>
-              </div>
-=======
-                <div className="input-field col s6">
-                    <select onChange={this.handleChangeGender.bind(this)}>
-                    <option selected>Gender</option>
-                   // <option value="" disabled selected>Choose your option</option>
-                    <option value="1">Male</option>
-                    <option value="2">Female</option>
-                    </select>
-                    <label>Gender</label>
-                </div>
->>>>>>> 5958bcdb9920a6fe947c174cc80d279dcee223cc
-
-                <div>
-                    <button type="button" className="button" onClick={this.handleSearch.bind(this)}>Search</button>
-                </div>
-            </div>
-
-
-          </form>
                     <div className="card-content">
-
-                    <CardsConnect/>
+                        <CardsConnect/>
                     </div>
 
-          </div>
+                 </div>
         );
     }
 }
